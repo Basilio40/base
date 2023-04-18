195061daff32321904197902f35b59918f33dc76
sandrobispo <sbispo@otimaeg.com.br>
2023-04-18 19:47:57 -0300

    Ajustes de estrutura modelos novos

--- app/urls.py
+++ app/urls.py
@@ -25,16 +25,44 @@ urlpatterns = [
     path('corpo_prova/<str:id>', views.corpo_prova, name='corpo_prova'),
     #Escopo
     path('escopo/<str:id>', views.escopo, name='escopo'),
+    path('serem_executados/<str:id>', views.serem_executados, name='serem_executados'),
+    path('obrigacoes/<str:id>', views.obrigacoes, name='obrigacoes'),
     #Contratos
     path('contratos/<str:id>', views.contratos, name='contratos'),
+    path('construtora_cliente/<str:id>', views.construtora_cliente, name='construtora_cliente'),
+    path('construtora_terceiro/<str:id>', views.construtora_terceiro, name='construtora_terceiro'),
+    path('construtora_fornecedor/<str:id>', views.construtora_fornecedor, name='construtora_fornecedor'),
     #Cronograma
     path('cronogramas/<str:id>', views.cronogramas, name='cronogramas'),
+    path('fisico_financeiro/<str:id>', views.fisico_financeiro, name='fisico_financeiro'),
+    path('fisico/<str:id>', views.fisico, name='fisico'),
+    path('financeiro/<str:id>', views.financeiro, name='financeiro'),
+    path('notificacoes/<str:id>', views.notificacoes, name='notificacoes'),
     #Compras
     path('compras/<str:id>', views.compras, name='compras'),
+    path('solicitacao_de_compras/<str:id>', views.solicitacao_de_compras, name='solicitacao_de_compras'),
+    path('cadernos_tecnicos/<str:id>', views.cadernos_tecnicos, name='cadernos_tecnicos'),
+    path('termos_garantia/<str:id>', views.termos_garantia, name='termos_garantia'),
+    path('memorial_calculo/<str:id>', views.memorial_calculo, name='memorial_calculo'),
+    path('logistica/<str:id>', views.logistica, name='logistica'),
+    path('historico/<str:id>', views.historico, name='historico'),
+    path('tabela_preco/<str:id>', views.tabela_preco, name='tabela_preco'),
+    path('romaneio/<str:id>', views.romaneio, name='romaneio'),
+    path('status_compra/<str:id>', views.status_compra, name='status_compra'),
     #Serviços
     path('servicos/<str:id>', views.servicos, name='servicos'),
+    path('instrucao_tecnica/<str:id>', views.instrucao_tecnica, name='instrucao_tecnica'),
+    path('ficha_verificacao/<str:id>', views.ficha_verificacao, name='ficha_verificacao'),
+    path('indicador_produtividade/<str:id>', views.indicador_produtividade, name='indicador_produtividade'),
+    path('previsao/<str:id>', views.previsao, name='previsao'),
+    path('checklist_visita/<str:id>', views.checklist_visita, name='checklist_visita'),
+    path('plano_macro/<str:id>', views.plano_macro, name='plano_macro'),
+    path('plano_micro/<str:id>', views.plano_micro, name='plano_micro'),
     #Portifolio
     path('portifolio/<str:id>', views.portifolio, name='portifolio'),
+    path('fotos/<str:id>', views.fotos, name='fotos'),
+    path('videos/<str:id>', views.videos, name='videos'),
+    path('informacoes_contato/<str:id>', views.informacoes_contato, name='informacoes_contato'),
    
     
     
--- app/views.py
+++ app/views.py
@@ -156,6 +156,237 @@ def corpo_prova(request,id):
     }
     return render(request, 'app/corpo_prova.html',context)
 
+#Escopo
+def serem_executados(request,id):
+    serem_executados = Obras.objects.filter(id=id)
+    context = {
+        'serem_executados':serem_executados,
+        'id':id
+    }
+    return render(request, 'app/serem_executados.html',context)
+
+def obrigacoes(request,id):
+    obrigacoes = Obras.objects.filter(id=id)
+    context = {
+        'obrigacoes':obrigacoes,
+        'id':id
+    }
+    return render(request, 'app/obrigacoes.html',context)
+#Contratos
+def construtora_cliente(request,id):
+    construtora_cliente = Obras.objects.filter(id=id)
+    context = {
+        'construtora_cliente':construtora_cliente,
+        'id':id
+    }
+    return render(request, 'app/construtora_cliente.html',context)
+
+def construtora_terceiro(request,id):
+    construtora_terceiro = Obras.objects.filter(id=id)
+    context = {
+        'construtora_terceiro':construtora_terceiro,
+        'id':id
+    }
+    return render(request, 'app/construtora_terceiro.html',context)
+
+def construtora_fornecedor(request,id):
+    construtora_fornecedor = Obras.objects.filter(id=id)
+    context = {
+        'construtora_fornecedor':construtora_fornecedor,
+        'id':id
+    }
+    return render(request, 'app/construtora_fornecedor.html',context)
+
+#Cronogramas
+def fisico_financeiro(request,id):
+    fisico_financeiro = Obras.objects.filter(id=id)
+    context = {
+        'fisico_financeiro':fisico_financeiro,
+        'id':id
+    }
+    return render(request, 'app/fisico_financeiro.html',context)
+
+def fisico(request,id):
+    fisico = Obras.objects.filter(id=id)
+    context = {
+        'fisico':fisico,
+        'id':id
+    }
+    return render(request, 'app/fisico.html',context)
+
+def financeiro(request,id):
+    financeiro = Obras.objects.filter(id=id)
+    context = {
+        'financeiro':financeiro,
+        'id':id
+    }
+    return render(request, 'app/financeiro.html',context)
+
+def notificacoes(request,id):
+    notificacoes = Obras.objects.filter(id=id)
+    context = {
+        'notificacoes':notificacoes,
+        'id':id
+    }
+    return render(request, 'app/notificacoes.html',context)
+
+#Compras
+
+def solicitacao_de_compras(request,id):
+    solicitacao_de_compras = Obras.objects.filter(id=id)
+    context = {
+        'solicitacao_de_compras':solicitacao_de_compras,
+        'id':id
+    }
+    return render(request, 'app/solicitacao_de_compras.html',context)
+
+def cadernos_tecnicos(request,id):
+    cadernos_tecnicos = Obras.objects.filter(id=id)
+    context = {
+        'cadernos_tecnicos':cadernos_tecnicos,
+        'id':id
+    }
+    return render(request, 'app/cadernos_tecnicos.html',context)
+
+def termos_garantia(request,id):
+    termos_garantia = Obras.objects.filter(id=id)
+    context = {
+        'termos_garantia':termos_garantia,
+        'id':id
+    }
+    return render(request, 'app/termos_garantia.html',context)
+
+def memorial_calculo(request,id):
+    memorial_calculo = Obras.objects.filter(id=id)
+    context = {
+        'memorial_calculo':memorial_calculo,
+        'id':id
+    }
+    return render(request, 'app/memorial_calculo.html',context)
+
+def logistica(request,id):
+    logistica = Obras.objects.filter(id=id)
+    context = {
+        'logistica':logistica,
+        'id':id
+    }
+    return render(request, 'app/logistica.html',context)
+
+def historico(request,id):
+    historico = Obras.objects.filter(id=id)
+    context = {
+        'historico':historico,
+        'id':id
+    }
+    return render(request, 'app/historico.html',context)
+
+def tabela_preco(request,id):
+    tabela_preco = Obras.objects.filter(id=id)
+    context = {
+        'tabela_preco':tabela_preco,
+        'id':id
+    }
+    return render(request, 'app/tabela_preco.html',context)
+
+def romaneio(request,id):
+    romaneio = Obras.objects.filter(id=id)
+    context = {
+        'romaneio':romaneio,
+        'id':id
+    }
+    return render(request, 'app/romaneio.html',context)
+
+def status_compra(request,id):
+    status_compra = Obras.objects.filter(id=id)
+    context = {
+        'status_compra':status_compra,
+        'id':id
+    }
+    return render(request, 'app/status_compra.html',context)
+
+#Serviços
+def instrucao_tecnica(request,id):
+    instrucao_tecnica = Obras.objects.filter(id=id)
+    context = {
+        'instrucao_tecnica':instrucao_tecnica,
+        'id':id
+    }
+    return render(request, 'app/instrucao_tecnica.html',context)
+
+def ficha_verificacao(request,id):
+    ficha_verificacao = Obras.objects.filter(id=id)
+    context = {
+        'ficha_verificacao':ficha_verificacao,
+        'id':id
+    }
+    return render(request, 'app/ficha_verificacao.html',context)
+
+def indicador_produtividade(request,id):
+    indicador_produtividade = Obras.objects.filter(id=id)
+    context = {
+        'indicador_produtividade':indicador_produtividade,
+        'id':id
+    }
+    return render(request, 'app/indicador_produtividade.html',context)
+
+def previsao(request,id):
+    previsao = Obras.objects.filter(id=id)
+    context = {
+        'previsao':previsao,
+        'id':id
+    }
+    return render(request, 'app/previsao.html',context)
+
+def checklist_visita(request,id):
+    checklist_visita = Obras.objects.filter(id=id)
+    context = {
+        'checklist_visita':checklist_visita,
+        'id':id
+    }
+    return render(request, 'app/checklist_visita.html',context)
+
+def plano_macro(request,id):
+    plano_macro = Obras.objects.filter(id=id)
+    context = {
+        'plano_macro':plano_macro,
+        'id':id
+    }
+    return render(request, 'app/plano_macro.html',context)
+
+def plano_micro(request,id):
+    plano_micro = Obras.objects.filter(id=id)
+    context = {
+        'plano_micro':plano_micro,
+        'id':id
+    }
+    return render(request, 'app/plano_micro.html',context)
+
+
+#Portifolio
+def fotos(request,id):
+    fotos = Obras.objects.filter(id=id)
+    context = {
+        'fotos':fotos,
+        'id':id
+    }
+    return render(request, 'app/fotos.html',context)
+
+def videos(request,id):
+    videos = Obras.objects.filter(id=id)
+    context = {
+        'videos':videos,
+        'id':id
+    }
+    return render(request, 'app/videos.html',context)
+
+def informacoes_contato(request,id):
+    informacoes_contato = Obras.objects.filter(id=id)
+    context = {
+        'informacoes_contato':informacoes_contato,
+        'id':id
+    }
+    return render(request, 'app/informacoes_contato.html',context)
+
 
 def sair(request):
     auth.logout(request)
