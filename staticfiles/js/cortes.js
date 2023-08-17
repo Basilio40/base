6aec03339abb87c6a236668bdc60d358a5424ef4
sandrobispo <sbispo@otimaeg.com.br>
2023-08-16 21:35:20 -0300

    collectstatic

---
+++ staticfiles/js/cortes.js
@@ -0,0 +1,151 @@
+function add_carro(){
+    container = document.getElementById('form-carro')
+    
+    n = document.querySelectorAll("input[type=file]").length
+    html = "<br>  " +
+           "<div class='row'> <div class='col-md'> " +
+           "<input type='text' id='img_descricao' placeholder='descrição' class='form-control' name='descricao' ></div>" +
+
+           "<div class='col-md'>"+
+           "<input type='date' id='img_data' placeholder='data' class='form-control' name='data' ></div>" +
+
+           "<div><label for='fileupload"+ n + "' class='custom-file-upload'><i class='fal fa-file'></i> Escolha um Ficheiro</label>" +
+           "<input type='file' id='fileupload"+ n + "' placeholder='Select file' name='imagem"+ n + "' multiple='multiple'></div></div> </div>"
+
+    container.innerHTML += html
+}
+
+document.querySelector("input.btn-principal").addEventListener("click", post_img_form)
+function post_img_form(e){
+    e.preventDefault();
+    
+    form = document.querySelector('form');
+    url = new URL(form.action);
+    formData = new FormData(form);
+
+    fetchOptions = {
+        method: form.method,
+        body: formData,
+    };
+
+    fetch(url, fetchOptions);
+}
+
+function exibir_form(tipo){
+
+    add_cliente = document.getElementById('adicionar-cliente')
+    att_cliente = document.getElementById('att_cliente')
+
+    if(tipo == "1"){
+        att_cliente.style.display = "none"
+        add_cliente.style.display = "block"
+
+    }else if(tipo == "2"){
+        add_cliente.style.display = "none";
+        att_cliente.style.display = "block"
+    }
+
+}
+
+
+function dados_cliente(){
+    cliente = document.getElementById('cliente-select')
+    csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+    id_cliente = cliente.value
+
+    data = new FormData()
+    data.append('id_cliente', id_cliente)
+
+    fetch("/clientes/atualiza_cliente/",{
+        method: "POST",
+        headers: {
+            'X-CSRFToken': csrf_token,
+        },
+        body: data
+
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+        document.getElementById('form-att-cliente').style.display = 'block'
+        
+        id = document.getElementById('id')
+        id.value = data['cliente_id']
+
+        nome = document.getElementById('nome')
+        nome.value = data['cliente']['nome']
+
+        sobrenome = document.getElementById('sobrenome')
+        sobrenome.value = data['cliente']['sobrenome']
+
+        cpf = document.getElementById('cpf')
+        cpf.value = data['cliente']['cpf']
+
+        email = document.getElementById('email')
+        email.value = data['cliente']['email']
+
+        div_carros = document.getElementById('carros')
+
+        for(i=0; i<data['carros'].length; i++){
+            div_carros.innerHTML += "\<form action='/clientes/update_carro/" + data['carros'][i]['id'] +"' method='POST'>\
+                <div class='row'>\
+                        <div class='col-md'>\
+                            <input class='form-control' name='carro' type='text' value='" + data['carros'][i]['fields']['carro'] + "'>\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='form-control' name='placa' type='text' value='" + data['carros'][i]['fields']['placa'] + "'>\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='form-control' type='text' name='ano' value='" + data['carros'][i]['fields']['ano'] + "' >\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='btn btn-lg btn-success' type='submit'>\
+                        </div>\
+                    </form>\
+                    <div class='col-md'>\
+                        <a href='/clientes/excluir_carro/"+ data['carros'][i]['id'] +"' class='btn btn-lg btn-danger'>EXCLUIR</a>\
+                    </div>\
+                </div><br>"
+        }
+        
+    })
+
+
+}
+
+
+function update_cliente(){
+    nome = document.getElementById('nome').value
+    sobrenome = document.getElementById('sobrenome').value
+    email = document.getElementById('email').value
+    cpf = document.getElementById('cpf').value
+    id = document.getElementById('id').value
+
+    fetch('/clientes/update_cliente/' + id, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrf_token,
+        },
+        body: JSON.stringify({
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            cpf: cpf,
+        })
+
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+
+        if(data['status'] == '200'){
+            nome = data['nome']
+            sobrenome = data['sobrenome']
+            email = data['email']
+            cpf = data['cpf']
+            console.log('Dados alterado com sucesso')
+        }else{
+            console.log('Ocorreu algum erro')
+        }
+
+    })
+
+}
